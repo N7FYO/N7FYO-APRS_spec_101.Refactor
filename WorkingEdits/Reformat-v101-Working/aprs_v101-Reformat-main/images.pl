5eee6c32dd7a4cde79ae97500fce7ffdc77cbf40
TahomaSoft:EHB <bacon@tahoma.com>
2023-08-28 10:37:47 -0400

    Added experimental html translation

---
+++ WorkingEdits/Reformat-v101-Working/aprs_v101-Reformat-main/images.pl
@@ -0,0 +1,30 @@
+# LaTeX2HTML 2021.2 (Released July 1, 2021)
+# Associate images original text with physical files.
+
+
+$key = q/!;MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ STYLE=""
+ SRC="|."$dir".q|img3.svg"
+ ALT="$!$">;WIDTH="0" HEIGHT="0"|; 
+
+$key = q/=;MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ STYLE=""
+ SRC="|."$dir".q|img4.svg"
+ ALT="$=$">;WIDTH="0" HEIGHT="0"|; 
+
+$key = q/Rightarrow;MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ STYLE=""
+ SRC="|."$dir".q|img1.svg"
+ ALT="$\Rightarrow$">;WIDTH="0" HEIGHT="0"|; 
+
+$key = q/dagger;MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ STYLE=""
+ SRC="|."$dir".q|img2.svg"
+ ALT="$\dagger$">;WIDTH="0" HEIGHT="0"|; 
+
+1;
+
